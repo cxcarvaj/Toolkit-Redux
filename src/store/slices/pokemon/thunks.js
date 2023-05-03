f37baaaf8f98c98b9f8b3595972496c4bc709616
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-05-03 16:44:24 -0500

    Implementing axios and creating the pokemonAPI file

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonAPI } from '../../../api/pokemonAPI';
 import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 export const getPokemons = (page = 0) => {
@@ -5,6 +6,18 @@ export const getPokemons = (page = 0) => {
     dispatch(startLoadingPokemons());
 
     // TODO: make a request to the API
-    // dispatch(setPokemons());
+    // const response = await fetch(
+    //   `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
+    // );
+    // const data = await response.json();
+    const { data } = await pokemonAPI.get(
+      `pokemon?limit=10&offset=${page * 10}`
+    );
+    dispatch(
+      setPokemons({
+        page: page + 1,
+        pokemons: data.results
+      })
+    );
   };
 };
